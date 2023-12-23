7e4bee44712699b798d29ffa71e3566c2ae9b414
Stefan <96178532+stefan6419846@users.noreply.github.com>
2023-12-23 20:51:45 +0100

    update metadata

--- setup.py
+++ setup.py
@@ -3,18 +3,18 @@
 from setuptools import find_packages, setup
 
 
-__version__ = '1.3.0'
+__version__ = '1.0.0'
 
 
 setup(
-    name="hocr-tools",
+    name='hocr-tools-lib',
     version=__version__,
     description='Advanced tools for hOCR integration',
-    author='Thomas Breuel',
-    maintainer='Konstantin Baierer',
-    maintainer_email='konstantin.baierer@gmail.com',
-    url='https://github.com/tmbdev/hocr-tools',
-    download_url='https://github.com/tmbdev/hocr-tools/tarball/v' + __version__,
+    author='Thomas Breuel, stefan6419846 (library version)',
+    # maintainer='Konstantin Baierer',
+    # maintainer_email='konstantin.baierer@gmail.com',
+    url='https://github.com/stefan6419846/hocr-tools/',
+    download_url='https://github.com/stefan6419846/hocr-tools/tarball/v' + __version__,
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Environment :: Console',
@@ -22,12 +22,6 @@ setup(
         'License :: OSI Approved :: Apache Software License',
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 3 :: Only',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
-        'Programming Language :: Python :: 3.11',
         'Topic :: Multimedia :: Graphics :: Graphics Conversion',
         'Topic :: Scientific/Engineering :: Image Recognition',
         'Topic :: Utilities',
