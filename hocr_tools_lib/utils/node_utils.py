7df016642595c8ec6abf6dd9fceec81da7cc91a6
Stefan <96178532+stefan6419846@users.noreply.github.com>
2023-12-30 19:41:42 +0100

    Update node_utils.py

--- hocr_tools_lib/utils/node_utils.py
+++ hocr_tools_lib/utils/node_utils.py
@@ -1,7 +1,14 @@
+from __future__ import annotations
+
 import re
+from typing import cast
+
+from lxml.html import HtmlElement
+
+from hocr_tools_lib.utils.rectangle_utils import RectangleType
 
 
-def get_prop(node, name, strip_value=False):
+def get_prop(node: HtmlElement, name: str, strip_value: bool = False) -> str | None:
     title = node.get('title')
     if not title:
         return None
@@ -11,18 +18,21 @@ def get_prop(node, name, strip_value=False):
         if strip_value:
             args = args.strip('"')
         if key == name:
-            return args
+            return cast(str, args)
     return None
 
 
-def get_bbox(node):
+def get_bbox(node: HtmlElement) -> RectangleType | None:
     bbox = get_prop(node, 'bbox')
     if not bbox:
         return None
-    return tuple([int(x) for x in bbox.split()])
+    return cast(
+        RectangleType,
+        tuple([int(x) for x in bbox.split()])
+    )
 
 
-def get_text(node):
+def get_text(node: HtmlElement) -> str:
     text_nodes = node.xpath(".//text()")
     s = "".join([text for text in text_nodes])
     return re.sub(r'\s+', ' ', s)
