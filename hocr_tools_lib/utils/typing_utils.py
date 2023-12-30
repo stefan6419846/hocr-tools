f3f41d72e82287091750454308d10dfdaf8fd8d1
Stefan <96178532+stefan6419846@users.noreply.github.com>
2023-12-30 19:51:34 +0100

    Create typing_utils.py

---
+++ hocr_tools_lib/utils/typing_utils.py
@@ -0,0 +1,15 @@
+try:
+    from _typeshed import SupportsRead
+except ImportError:
+    # Re-implementation of the original version.
+    from typing import Protocol, TypeVar
+
+    _T_co = TypeVar("_T_co", covariant=True)
+
+    class SupportsRead(Protocol[_T_co]):  # type: ignore[no-redef]
+        def read(self, __length: int = ...) -> _T_co: ...
+
+
+__all__ = [
+    "SupportsRead",
+]
