cc4c8af2d30aaeeba8fe8f08daaafb84823140ea
Stefan <96178532+stefan6419846@users.noreply.github.com>
2023-12-31 11:47:41 +0100

    Update __init__.py

--- tests/__init__.py
+++ tests/__init__.py
@@ -27,7 +27,7 @@ class TestCase(_TestCase):
     @classmethod
     def get_data_content(cls, path: str) -> bytes:
         reference = importlib_resources.files('tests.data') / path
-        return cast(bytes, reference.read_bytes())  # `cast` only required for Python < 3.10.
+        return cast(bytes, reference.read_bytes())  # `cast` only required for Python < 3.10.  # type: ignore[redundant-cast]
 
     @classmethod
     def get_data_directory(cls) -> Path:
